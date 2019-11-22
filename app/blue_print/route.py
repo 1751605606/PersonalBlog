1e38a881c600ffaa0c5493bddcc38c4e2b412a34
xwl <16302010030@fudan.edu.cn>
2019-11-22 21:16:53 +0800

    model change

--- app/blue_print/route.py
+++ app/blue_print/route.py
@@ -20,7 +20,7 @@ def user_register():
         user = db.session.query(User).filter(User.username == username).first()
         # 用户名未被使用
         if user is None:
-            user = User(username, password)
+            user = User(username, password, 0)
             try:
                 user.add_user()
             # 发生异常
@@ -53,44 +53,46 @@ def user_register():
             }
     return render_template("register.html")
 
-# 单用户版本
-@blue_print.route('/api/user/login', methods=['POST', 'GET'])
-def user_login():
-    if request.method == 'POST':
-        json_data = json.loads(request.get_data())
-        username = json_data.get("username")
-        password = json_data.get("password")
-        if username == 'admin' and password == 'admin':
-            session['username'] = username
-            return {
-                "code": "200",
-                "error": {},
-                "data": {
-                    "token": "token"
-                }
-            }
-        else:
-            return {
-                "code": "200",
-                "error": {
-                    "type": "user not found",
-                    "message": "no such user: " + username
-                },
-                "data": {}
-            }
-    if 'username' in session:
-        return {
-                "code": "200",
-                "error": {},
-                "data": {
-                    "token": "token"
-                }
-            }
-    return render_template("login.html")
+#  #单用户版本
+# @blue_print.route('/api/user/login_1', methods=['POST', 'GET'])
+# def user_login_1():
+#     if request.method == 'POST':
+#         json_data = json.loads(request.get_data())
+#         username = json_data.get("username")
+#         password = json_data.get("password")
+#         if username == 'admin' and password == 'admin':
+#             session['username'] = username
+#             return {
+#                 "code": "200",
+#                 "error": {},
+#                 "data": {
+#                     "token": "token"
+#                 }
+#             }
+#         else:
+#             return {
+#                 "code": "200",
+#                 "error": {
+#                     "type": "user not found",
+#                     "message": "no such user: " + username
+#                 },
+#                 "data": {}
+#             }
+#     if 'username' in session:
+#         return {
+#                 "code": "200",
+#                 "error": {},
+#                 "data": {
+#                     "token": "token"
+#                 }
+#             }
+#     return render_template("login.html")
+
 
 # 多用户版本
-@blue_print.route('/api/user/login_1', methods=['POST', 'GET'])
-def user_login_1():
+
+@blue_print.route('/api/user/login', methods=['POST', 'GET'])
+def user_login():
     if request.method == 'POST':
         json_data = json.loads(request.get_data())
         username = json_data.get("username")
@@ -107,10 +109,13 @@ def user_login_1():
             }
         else:
             session['username'] = username
+            group_id = "1" if username == 'admin' else "0"
             return {
                 "code": "200",
                 "error": {},
                 "data": {
+                    "id": user.id,
+                    "group_id": group_id,
                     "token": "token"
                 }
             }
@@ -234,5 +239,9 @@ def get_articles():
                 'text':  article.text
             }
             articles_list.append(article)
-        response = {'code': '200', 'error': {}, 'data': articles_list}
-        return json.dumps(request)
+        response = {
+            'code': '200',
+            'error': {},
+            'data': articles_list
+        }
+        return json.dumps(response)
--- app/models.py
+++ app/models.py
@@ -6,15 +6,21 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    group_id = db.Column(db.Integer)
     # articles = db.relationship('Article', backref='user', lazy='dynamic')
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, group_id):
         self.username = username
         self.password = password
+        self.group_id = group_id
 
     def add_user(self):
         with db.auto_commit_db():
             db.session.add(self)
+
+    def update_user(self):
+        with db.auto_commit_db():
+            pass
 
 
 class Article(db.Model):
@@ -23,11 +29,17 @@ class Article(db.Model):
     # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(100))
     text = db.Column(db.Text)
+    view_number = db.Column(db.Integer)
+    like_number = db.Column(db.Integer)
+    comment_number = db.Column(db.Integer)
 
-    def __init__(self, id, title, text):
+    def __init__(self, id, title, text, view_number, like_number, comment_number):
         self.id = id
         self.title = title
         self.text = text
+        self.view_number = view_number
+        self.like_number = like_number
+        self.comment_number = comment_number
 
     def add_article(self):
         with db.auto_commit_db():
